facc19714cbf6548f194f1c51c239bd737ff92b8
Sean Sarokas <sps4462@gmail.com>
2022-04-20 21:04:37 -0400

    commit tree d416c0a50cfda49ecbae045799b4b4667d2f1351

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+  function superbowlWin(array){
+    let winFound;
+    if (array.find(item => item.result === "W")){
+    winFound = (array.find(item => item.result === "W"))
+      return winFound.year
+    }
+  }
